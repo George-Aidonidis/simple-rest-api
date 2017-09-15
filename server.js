e4c1a2706c42566bac18cfb8c84d0e65fe037e4c
George Aidonidis <george.aidonidis@gmail.com>
2017-09-15 23:31:10 +0300

    Test all crud routes
    closes #3

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = config.port;
 
 app.get('/', (req, res) => Note.find({})
-    .then(notes => res.send(notes))
-    .catch(err => {
-      console.error(err);
-      return res.status(500).send('There was a problem finding the notes');
-    })
+  .then(notes => res.send(notes))
+  .catch(err => {
+    console.error(err);
+    return res.status(500).send('There was a problem finding the notes');
+  })
 );
 
 app.get('/note/:id', (req, res) => {
@@ -36,26 +36,25 @@ app.post('/note', (req, res) => {
   const content = req.body.content;
   if (title && content) {
     return Note.create(req.body)
-      .then(res.status(200).send('Note created'))
+      .then(note => res.status(200).send(note))
       .catch(console.error);
   }
   return res.status(400).send('Bad request');
 });
 
 app.patch('/note/:id', (req, res) => {
-  const title = req.body.title;
-  const content = req.body.content;
-  if (title && content) {
-    return Note.update(req.body)
-      .then(res.status(200).send('Note updated'))
+  const id = req.params.id;
+  if (req.body.title || req.body.content) {
+    return Note.findByIdAndUpdate(id, { $set: req.body }, { new: true })
+      .then(updatedNote => res.status(200).send(updatedNote))
       .catch(console.error);
   }
   return res.status(400).send('Bad request');
 });
 
 app.delete('/note/:id', (req, res) => Note.findByIdAndRemove(req.params.id)
-    .then(res.status(200).send('Note removed'))
-    .catch(console.error)
+  .then(res.status(200).send('Note removed'))
+  .catch(console.error)
 );
 
 
--- test/app.test.js
+++ test/app.test.js
@@ -2,18 +2,56 @@
 
 const chai = require('chai');
 const request = require('supertest');
+const mongoose = require('mongoose');
 
 const expect = chai.expect;
-
-/* eslint  no-unused-expressions: "off", global-require: "off" */
+const dummyObject = {
+  title: 'myTitle',
+  content: 'some content here'
+};
+/* eslint  no-unused-expressions: "off",
+  global-require: "off",
+  no-underscore-dangle: off */
 describe('server', () => {
+  let noteId;
+  before((done) => {
+    mongoose.connection.db.dropDatabase(done);
+  });
   const server = require('../server');
   it('server.js file exists', () => {
     expect(server).to.exist;
   });
-  it('responds to /', (done) => {
-    request(server)
-      .get('/')
-      .expect(200, done);
-  });
+  it('POST /note', () => request(server).post('/note')
+    .send(dummyObject)
+    .expect(200)
+    .then(res => {
+      noteId = res.body._id;
+      expect(res.body.title).to.equal(dummyObject.title);
+      expect(res.body.content).to.equal(dummyObject.content);
+    })
+  );
+  it('GET /', () => request(server).get('/')
+    .expect(200)
+    .then(res => {
+      expect(res.body).to.be.an('Array');
+      expect(res.body[0].title).to.equal(dummyObject.title);
+      expect(res.body[0].content).to.equal(dummyObject.content);
+    })
+  );
+  it('GET /note/:id', () => request(server).get(`/note/${noteId}`)
+    .expect(200)
+    .then(res => {
+      expect(res.body.title).to.equal(dummyObject.title);
+      expect(res.body.content).to.equal(dummyObject.content);
+    })
+  );
+  it('PATCH /note/:id', () => request(server).patch(`/note/${noteId}`)
+    .send({ title: 'updatedTitle' })
+    .expect(200)
+    .then(res => {
+      expect(res.body.title).to.equal('updatedTitle');
+      expect(res.body.content).to.equal(dummyObject.content);
+    })
+  );
+  it('DELETE /note/:id', () => request(server).delete(`/note/${noteId}`).expect(200));
 });
